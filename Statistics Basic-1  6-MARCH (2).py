f0a1a54da0831cc28d7a7ba1643bd44fb29f925a
afrazahmad84 <114479304+afrazahmad84@users.noreply.github.com>
2023-04-08 17:19:43 +0530

    Add files via upload

---
+++ Statistics Basic-1  6-MARCH (2).py
@@ -0,0 +1,99 @@
+#!/usr/bin/env python
+# coding: utf-8
+Q1. What is Statistics?
+
+Ans:-
+Statistics is the science  collecting, organizing and analyzing, interpreting and presenting the data.Q2. Define the different types of statistics and give an example of when each type might be used.
+
+Ans:-
+There are two types of statistics 
+1-Descritive Statistics
+2-inferential Statistics
+
+1-Descritive Statistics:-it consists of organizing and summarizing the data in Descritive Statistics we will use to calculate mean, median and mod
+2-inferential Statistics:-it consists of using data you have measured from the conclusion for the data. Q3. What are the different types of data and how do they differ from each other? Provide an example of each type of data.
+
+Ans:-
+There are two types of data type
+1. Qualitative data 
+a.Nominal data
+eg:- Gender (Women, Men)
+b.ordinal
+eg:-Letter grades: A, B, C, and etc.
+2.Quantitative data
+a.Discrete data
+eg:-The number of students in a class
+b.continuous
+eg:-Daily temperature recordings of a placeQ4. Categorise the following datasets with respect to quantitative and qualitative data types:
+(i) Grading in exam: A+, A, B+, B, C+, C, D, E
+(ii) Colour of mangoes: yellow, green, orange, red
+(iii) Height data of a class: [178.9, 179, 179.5, 176, 177.2, 178.3, 175.8,...]
+(iv) Number of mangoes exported by a farm: [500, 600, 478, 672, ...]
+
+Ans:-
+(i) Qualitative
+(ii) Qualitative 
+(iii) Quantitative
+(iv) QuantitativeQ5. Explain the concept of levels of measurement and give an example of a variable for each level.
+
+(i) Nominal scale data
+(ii) Ordinal scale data
+(iii) Interval scale data
+(iv) Ratio scale Data
+
+(i) Nominal scale data:-
+1.Qualitative/categorical
+2. eg. gender,colors
+3.order does not matter
+(ii) Ordinal Scale Data:-
+1. Ranking is important
+2.order metter
+Difference cannot be measured 
+eg:-Best
+    good
+    bad
+(iii) (iii) Interval scale data:-
+1.the order matter
+2.Difference can be measured
+3.ratio cannot be measured
+4.No True "o" starting point
+(iv) Ratio scale Data:-
+1 The order matter
+2.Differences are measurable
+
+
+Q6. Why is it important to understand the level of measurement when analyzing data? Provide an
+example to illustrate your answer.
+
+Ans:-
+Level of measurement, also known as scale of measurement, refers to the process of categorizing data based on the characteristics and properties of the data. It is important in statistics because it helps determine the appropriate statistical methods and tests that can be used to analyze the data.
+
+
+# In[ ]:
+
+
+
+
+Q7. How nominal data type is different from ordinal data type.
+Ans:-
+Different between nominal data type and ordinal data type
+In the nominal data type does not matter the order but ordinal data type always follow the order.
+Q8. Which type of plot can be used to display data in terms of range?
+
+Ans:-
+A histogram is a specific type of bar chart, where the categories are ranges of numbers. Histograms therefore show combined continuous data.
+
+Q9. Describe the difference between descriptive and inferential statistics. Give an example of each
+type of statistics and explain how they are used.
+
+Ans:-
+descriptive statistics:- Descriptive statistics explains already known data related to a particular sample or population of a small size
+eg:-student's grade point average (GPA)
+
+Inferential statistics helps to compare data, make hypotheses and predictions.
+eg:-making estimates about populations
+Q10. What are some common measures of central tendency and variability used in statistics? Explain
+how each measure can be used to describe a dataset.
+
+Ans:-
+these measures include the mean, median, and mode. These statistics indicate where most values in a distribution fall and are also referred to as the central location of a distribution. You can think of central tendency as the propensity for data points to cluster around a middle value
